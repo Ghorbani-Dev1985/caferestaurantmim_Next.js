714ab86a38d43b62164da462ac756b6e548e29c7
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-07-16 18:23:46 +0330

    feat: develop desktop menu

--- src/common/Header/Header.tsx
+++ src/common/Header/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 import {
+  Divider,
   Navbar,
   NavbarBrand,
   NavbarContent,
@@ -12,14 +13,12 @@ import { GetMainMenu } from "src/services/MenuService";
 import { useGetMenu } from "src/hooks/useMenu";
 import { MenuListType } from "src/types/menu";
 import React from "react";
+import { HiChevronDown } from "react-icons/hi";
 
 const Header = () => {
   const { data: mainMenu, isPending } = useGetMenu();
-  const parentMenus = mainMenu?.filter(
-    (menu: MenuListType) => menu.parent === 0
-  );
-
-  console.log(mainMenu, parentMenus);
+  const subMenu = mainMenu?.filter((menu: MenuListType) => menu.parent !== 0);
+  console.log(subMenu);
   return (
     <header>
       <TopHeader />
@@ -36,15 +35,42 @@ const Header = () => {
           />
         </NavbarBrand>
         <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          {parentMenus?.map(({ id, title, object_id, parent }: MenuListType) => {
+          {mainMenu?.map(({ id, title, object_id, parent }: MenuListType) => {
+            const subMenuItems = mainMenu.filter(
+              (menu: MenuListType) => menu.parent === id
+            );
             return (
-              <React.Fragment key={id}>
-                <NavbarItem>
-                  <Link color="foreground" href={`mimrestaurant/${object_id}`}>
-                    {title.rendered}
-                  </Link>
-                </NavbarItem>
-              </React.Fragment>
+              parent === 0 && (
+                <React.Fragment key={id}>
+                  <NavbarItem className="relative group flex-center">
+                    <Link color="foreground" href={`/menuItems/${object_id}`}>
+                      {title.rendered}
+                    </Link>
+                    {subMenuItems.length > 0 && (
+                      <div className="absolute flex flex-col invisible opacity-0 group-hover:visible group-hover:opacity-100 bg-white space-y-5 min-w-60 right-0 top-full p-3 rounded-xl shadow-lg border-b-5 border-b-primary xl:pt-4 transition-all z-50">
+                        {subMenuItems.map(
+                          ({ id, title, object_id }: MenuListType) => {
+                            return (
+                              <React.Fragment key={id}>
+                                <Link
+                                  color="foreground"
+                                  className="bg-primary-50/70 hover:bg-primary-500 hover:text-white p-2 rounded-lg transition-colors"
+                                  href={`/menuItems/${object_id}`}
+                                >
+                                  {title.rendered}
+                                </Link>
+                              </React.Fragment>
+                            );
+                          }
+                        )}
+                      </div>
+                    )}
+                    {subMenuItems.length > 0 && (
+                      <HiChevronDown className="size-5" />
+                    )}
+                  </NavbarItem>
+                </React.Fragment>
+              )
             );
           })}
         </NavbarContent>
