e055ab40a90b4bc0b5812c87dfa0a36191ffb3f5
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-07-22 20:33:03 +0330

    build & cheack error

--- src/app/(users)/page.tsx
+++ src/app/(users)/page.tsx
@@ -1,31 +1,18 @@
 
-import AboutUsItems from "./aboutus/AboutUsItems";
+import { Metadata } from "next";
 
-export const metadata = {
+
+export const metadata : Metadata = {
  title: "کافه و رستوران میم | Cafe Restaurant mim",
  description: "کافه و رستوران میم با محیطی زیبا در خیابان لاکانی رشت آماده پذیرایی شما عزیزان می باشد ، امیدواریم لحظات خوبی را کنار هم داشته باشیم"
 }
 
-export async function getBlogs() {
-  const { data } = await Http.get("/articles");
-  const filteredBlog = data.filter((blog : any) => blog.publish === true);
-  return filteredBlog;
-}
 
 const HomePage = async () => {
-  const blogs = await getBlogs();
   return (
     <>
       <main className="min-h-screen">
-        <Slider />
-        <section className="container">
-          <QuickAccess />
-          <Menus />
-          <AboutUsItems > </AboutUsItems>
-          <BlogsList blogs={blogs} />
-        </section>
-        <AboutUS />
-        <ImageGallery />
+       main
       </main>
     </>
   );
