8e078256eb5302204fccfbc097ae5f26cfd1770e
ghorbani-dev1985 <ghorbani.dev1985@gmail.com>
2024-07-25 19:31:50 +0330

    feat: develop reserve section in main page

--- src/app/(users)/page.tsx
+++ src/app/(users)/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from "next";
 import MainSlider from "src/components/Home/MainSlider";
 import QuickAccess from "src/components/Home/quickAccess";
 import AboutUs from "src/components/Home/AboutUs";
+import Reserve from "src/components/Home/Reserve";
 export const metadata : Metadata = {
  title: "کافه و رستوران میم | Cafe Restaurant mim",
  description: "کافه و رستوران میم با محیطی زیبا در خیابان لاکانی رشت آماده پذیرایی شما عزیزان می باشد ، امیدواریم لحظات خوبی را کنار هم داشته باشیم"
@@ -14,6 +15,7 @@ const HomePage = async () => {
   <MainSlider />
   <QuickAccess />
    <AboutUs />
+   <Reserve />
     </>
   )
 };
---
+++ src/components/Home/Reserve.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { HiOutlineArrowLongLeft } from "react-icons/hi2";
+
+const Reserve = () => {
+  return (
+    <section className="container">
+      <div className="flex-col md:flex-row flex-between h-40 md:h-32 bg-gradient-to-l from-primary-400 to-primary-700 text-white rounded-3xl my-9 px-4 py-5 md:py-2">
+        <p className="font-extrabold text-base text-center md:text-lg animate-bounce pt-7 md:pt-0">
+          می تونی میزنو تو کافه رستوران میم همین حالا رزور کنی
+        </p>
+        <Link
+          href="/reservation"
+          className="flex-center gap-x-1.5 bg-white hover:text-white border hover:border-white hover:bg-transparent text-lg font-bold text-primary px-5 py-2 rounded-lg transition-colors"
+        >
+          <HiOutlineArrowLongLeft className="size-5" />
+          <span> رزرو میز</span>
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Reserve;
